e1d6462797cc3e66b9d2553bf3cdb92b7182bb2a
Даниил Косенко <daniilkosenko80@yandex.ru>
2026-04-07 20:35:42 +0300

    add smart-selects

--- PMK/PMK/settings.py
+++ PMK/PMK/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'list_lines.apps.ListLinesConfig',
     'core.apps.CoreConfig',
+    'smart_selects',
 ]
 
 MIDDLEWARE = [
--- PMK/list_lines/models.py
+++ PMK/list_lines/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from core.models import StatusCPUModel
+from smart_selects.db_fields import ChainedForeignKey
 
 
 # создаем список подразделений
@@ -18,7 +19,8 @@ class ListDepartment(models.Model):
 class ListLine(models.Model):
     title = models.CharField(max_length=100, verbose_name='Участок')
     department_type = models.ForeignKey(ListDepartment,
-                                        on_delete=models.CASCADE)
+                                        on_delete=models.CASCADE,
+                                        verbose_name='Отдел')
 
     class Meta:
         verbose_name = 'участок'
@@ -31,8 +33,9 @@ class ListLine(models.Model):
 # создаем список Системы внутри участков
 class SystemLine(models.Model):
     title = models.CharField(max_length=100, verbose_name='Система')
-    department_type = models.ForeignKey(ListLine,
-                                        on_delete=models.CASCADE)
+    line_type = models.ForeignKey(ListLine,
+                                  on_delete=models.CASCADE,
+                                  verbose_name='Участок')
 
     class Meta:
         verbose_name = 'системы'
@@ -62,12 +65,21 @@ class FrequencyConverterList(StatusCPUModel):
     manufacturer = models.ForeignKey(ManufacturerList,
                                      on_delete=models.CASCADE)
     is_status_programm = models.BooleanField(verbose_name='Параметры')
-    department_type = models.ForeignKey(SystemLine,
-                                        on_delete=models.CASCADE)
+    line_type = models.ForeignKey(ListLine,
+                                  on_delete=models.CASCADE,
+                                  verbose_name='Участок')
+    system_type = ChainedForeignKey(SystemLine,
+                                    chained_field="line_type",
+                                    chained_model_field="line_type",
+                                    show_all=False,
+                                    auto_choose=True,
+                                    sort=True,
+                                    on_delete=models.CASCADE,
+                                    verbose_name='Системы')
 
     class Meta:
         verbose_name = 'частотный преобразователь'
         verbose_name_plural = 'Частотные преобразователи'
 
     def __str__(self):
-        return self.title
+        return f'{self.line_type} — {self.system_type}'
